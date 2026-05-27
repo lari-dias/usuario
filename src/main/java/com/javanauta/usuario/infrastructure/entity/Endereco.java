b199e6da70834870e4611a92b6bc37e18d143006
Larissa <laridearaujo@hotmail.com>
2026-05-27 20:35:09 -0300

    corrige entidade Endereco, Telefone, repositories e application.properties

--- src/main/java/com/javanauta/usuario/infrastructure/entity/Endereco.java
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Endereco.java
@@ -10,29 +10,27 @@ import lombok.*;
 @Entity
 @Table(name = "endereco")
 @Builder
-
 public class Endereco {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "rua")
+    @Column(name = "rua", length = 150, nullable = false)
     private String rua;
 
-    @Column(name = "numero")
+    @Column(name = "numero", length = 20, nullable = false)
     private String numero;
 
-    @Column(name = "complemento", length = 10)
+    @Column(name = "complemento", length = 150)
     private String complemento;
 
-    @Column(name = "cidade", length = 150)
+    @Column(name = "cidade", length = 100, nullable = false)
     private String cidade;
 
-    @Column(name = "estado", length = 2)
+    @Column(name = "estado", length = 2, nullable = false)
     private String estado;
 
-    @Column(name = "cep", length = 9)
+    @Column(name = "cep", length = 9, nullable = false)
     private String cep;
-
 }
