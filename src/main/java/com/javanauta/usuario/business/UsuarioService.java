72db20f6e5a35b2f12fdd03d4442e45ff9e7c8c8
Larissa <laridearaujo@hotmail.com>
2026-05-28 17:30:09 -0300

    ajusta UsuarioService para criptografar senha com BCrypt

--- src/main/java/com/javanauta/usuario/business/UsuarioService.java
+++ src/main/java/com/javanauta/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.javanauta.usuario.infrastructure.entity.Usuario;
 import com.javanauta.usuario.infrastructure.exeptions.ResourceNotFoundExeption;
 import com.javanauta.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,19 +15,29 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
+
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
-        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+
+        // Criptografa a senha antes de salvar
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+        return usuarioConverter.paraUsuarioDTO(usuarioSalvo);
     }
 
     public Usuario buscarUsuarioPorEmail(String email) {
+
         return usuarioRepository.findByEmail(email)
                 .orElseThrow(() ->
                         new ResourceNotFoundExeption("Email não encontrado: " + email));
     }
 
     public void deletaUsuarioPorEmail(String email) {
+
         usuarioRepository.deleteByEmail(email);
     }
 }
